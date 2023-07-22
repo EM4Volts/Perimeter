d68d7c7fec0f19eeb11def55d23f104c0d7cfbce
EM4Volts <em4volts@gmail.com>
2023-07-22 15:20:41 +0200

    add license blocks

--- rpak.py
+++ rpak.py
@@ -1,7 +1,20 @@
+#     Blender addon for managing Northstar settings
+#     Copyright (C) 2023 EM4Volts
+#  
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#  
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#  
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import json, os, sys, shutil, subprocess
-
-
-
 
 def make_rpak_map( material_name, material_path, material_textures, repak_path ):
     preset_json ={
